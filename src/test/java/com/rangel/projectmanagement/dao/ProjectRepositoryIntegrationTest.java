007cdce871fed06546c66a92130f6ee0a328c7b3
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-05-27 00:31:35 -0300

    Removed unnecessary annotations

--- src/test/java/com/rangel/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/rangel/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
@@ -5,32 +5,17 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.rangel.projectmanagement.ProjectManagementApplication;
+
 import com.rangel.projectmanagement.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({
-    @Sql(
-        executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
-        scripts = {
-            "classpath:schema.sql",
-            "classpath:data.sql"
-        }
-    ),
-    @Sql(
-        executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
-        scripts = "classpath:drop.sql"
-    )
-})
 public class ProjectRepositoryIntegrationTest {
     
     @Autowired
@@ -41,6 +26,6 @@ public class ProjectRepositoryIntegrationTest {
         Project newProject = new Project("New Test Project", "COMPLETE", "Test description");
         proRepo.save(newProject);
 
-        assertEquals(5, proRepo.findAll().size());
+        assertEquals(1, proRepo.findAll().size());
     }
 }
