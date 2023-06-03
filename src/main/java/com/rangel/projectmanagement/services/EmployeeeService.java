19aab1a97af9f580b213ae4b1a85a68bcd721763
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-03 18:02:14 -0300

    Created a service as an abstration for employee reposity

---
+++ src/main/java/com/rangel/projectmanagement/services/EmployeeeService.java
@@ -0,0 +1,30 @@
+package com.rangel.projectmanagement.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rangel.projectmanagement.dao.EmployeeRepostory;
+import com.rangel.projectmanagement.dto.EmployeeProject;
+import com.rangel.projectmanagement.entities.Employee;
+
+
+@Service
+public class EmployeeeService {
+    
+    @Autowired
+    EmployeeRepostory empRepo;
+
+    public Employee save(Employee employee) {
+        return empRepo.save(employee);
+    }
+
+    public List<Employee> getAll() {
+        return empRepo.findAll();
+    }
+
+    public List<EmployeeProject> employeeProject() {
+        return empRepo.employeeProjects();
+    }
+}
