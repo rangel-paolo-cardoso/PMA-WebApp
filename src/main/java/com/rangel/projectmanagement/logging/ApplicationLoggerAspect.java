6c8363179aa41a176a133d1da51d49e1f845e849
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-06 22:01:20 -0300

    Added Advice to logging before and after each controller

--- src/main/java/com/rangel/projectmanagement/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/rangel/projectmanagement/logging/ApplicationLoggerAspect.java
@@ -1,6 +1,11 @@
 package com.rangel.projectmanagement.logging;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -14,14 +19,31 @@ public class ApplicationLoggerAspect {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("within(com.rangel.projectmanagement.controllers..*)"
-            + "|| within(com.rangel.projectmanagement.dao..*)")
+    @Pointcut("within(com.rangel.projectmanagement.controllers..*)")
     public void definePackagePointcuts() {
         // empty method just to name the location specified in the pointcut
     }
 
-    @Before("definePackagePointcuts()")
-    public void log() {
-        log.info(" -------------------------------------------- ");
+    @Around("definePackagePointcuts()")
+    public Object logAround(ProceedingJoinPoint jp) {
+        log.info(" \n \n \n ");
+        log.info("********** Before Method Execution ************ \n {}, {} () with arguments[s] = {}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.info("______________________________________________________ \n \n \n");
+
+        Object o = null;
+        try {
+            o = jp.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        log.info("********** After Method Execution ************ \n {}, {} () with arguments[s] = {}",
+                jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.info("______________________________________________________ \n \n \n");
+
+        return o;
     }
 }
