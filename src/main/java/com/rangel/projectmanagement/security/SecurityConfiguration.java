fae3b5cfee71031419aef0af0fce592da1799b0f
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-09 23:26:34 -0300

    Added Access Control

--- src/main/java/com/rangel/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/rangel/projectmanagement/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.rangel.projectmanagement.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,11 +22,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("rangel")
                 .password("pass2")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("managerUser")
+                .password("pass3")
+                .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/projects/new").hasRole("ADMIN")
+        .antMatchers("/employees/new").hasRole("ADMIN")
+        .antMatchers("/").authenticated().and().formLogin();
     }
 }
