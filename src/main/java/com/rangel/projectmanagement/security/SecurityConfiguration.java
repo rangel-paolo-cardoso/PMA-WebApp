ebdc5eb59492846c6fc4792b2ce190c1be332dda
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-11 21:40:33 -0300

    Added BCryptEncoder to encode passwords

--- src/main/java/com/rangel/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/rangel/projectmanagement/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,37 +20,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    BCryptPasswordEncoder bCryptEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser("myuser")
-                .password("pass")
-                .roles("USER")
-                .and()
-                .withUser("rangel")
-                .password("pass2")
-                .roles("USER")
-                .and()
-                .withUser("managerUser")
-                .password("pass3")
-                .roles("ADMIN");
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("select username, password, enabled "
+                        + "from user_accounts where username = ?")
+                .authoritiesByUsernameQuery("select username, role "
+                        + "from user_accounts where username = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/projects/save").hasRole("ADMIN")
                 .antMatchers("/employees/new").hasRole("ADMIN")
-                .antMatchers("/h2_console/**").permitAll()
-                .antMatchers("/").authenticated().and().formLogin();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+                .antMatchers("/employees/save").hasRole("ADMIN")
+                .antMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin();
     }
 }
