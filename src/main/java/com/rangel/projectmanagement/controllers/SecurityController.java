7affd1ef46cdf8bfdd026ddf550910ecb5787b1e
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-12 23:21:14 -0300

    Added an endpoint to save users.

--- src/main/java/com/rangel/projectmanagement/controllers/SecurityController.java
+++ src/main/java/com/rangel/projectmanagement/controllers/SecurityController.java
@@ -1,13 +1,23 @@
 package com.rangel.projectmanagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.rangel.projectmanagement.dao.UserAccountRepository;
 import com.rangel.projectmanagement.entities.UserAccount;
 
 @Controller
 public class SecurityController {
+
+    @Autowired
+    UserAccountRepository accountRepo;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptEncoder;
     
     @GetMapping("/register")
     public String register(Model model) {
@@ -15,5 +25,12 @@ public class SecurityController {
         model.addAttribute("userAccount", userAccount);
 
         return "security/register";
+    }
+
+    @PostMapping("/register/save")
+    public String saveUser(Model model, UserAccount user) {
+        user.setPassword(bCryptEncoder.encode(user.getPassword()));
+        accountRepo.save(user);
+        return "redirect:/";
     }
 }
