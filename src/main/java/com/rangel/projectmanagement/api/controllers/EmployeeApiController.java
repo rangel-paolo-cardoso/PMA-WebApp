0cc88562ab6854502eb7e80d293adab951c5b2a5
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-15 22:36:16 -0300

    Finished the Rest API for employees

--- src/main/java/com/rangel/projectmanagement/api/controllers/EmployeeApiController.java
+++ src/main/java/com/rangel/projectmanagement/api/controllers/EmployeeApiController.java
@@ -1,10 +1,14 @@
 package com.rangel.projectmanagement.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,8 +35,41 @@ public class EmployeeApiController {
     }
 
     @PostMapping(consumes = "application/json")
-    @ResponseStatus( HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee create(@RequestBody Employee employee) {
         return empRepo.save(employee);
+    }
+
+    @PutMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee update(@RequestBody Employee employee) {
+        return empRepo.save(employee);
+    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json")
+    public Employee partialUpdate(@PathVariable("id") Long id, @RequestBody Employee patchEmployee) {
+        Employee emp = empRepo.findById(id).get();
+
+        if (patchEmployee.getEmail() != null) {
+            emp.setEmail(patchEmployee.getEmail());
+        }
+        if (patchEmployee.getFirstName() != null) {
+            emp.setFirstName(patchEmployee.getFirstName());
+        }
+        if (patchEmployee.getLastName() != null) {
+            emp.setLastName(patchEmployee.getLastName());
+        }
+
+        return empRepo.save(emp);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        try {
+            empRepo.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            System.out.println("Already deleted!");
+        }
     }
 }
